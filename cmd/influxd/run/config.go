34b377edeacbea7135d701988d127a56eb076d8a
Jason Joo <hblzxsj@163.com>
2020-10-15 16:54:14 +0800

    Change default configuration of influxd
    Signed-off-by: Jason Joo <hblzxsj@163.com>

--- cmd/influxd/run/config.go
+++ cmd/influxd/run/config.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"os/user"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -110,20 +107,9 @@ func NewConfig() *Config {
 func NewDemoConfig() (*Config, error) {
 	c := NewConfig()
 
-	var homeDir string
-	// By default, store meta and data files in current users home directory
-	u, err := user.Current()
-	if err == nil {
-		homeDir = u.HomeDir
-	} else if os.Getenv("HOME") != "" {
-		homeDir = os.Getenv("HOME")
-	} else {
-		return nil, fmt.Errorf("failed to determine current user for storage")
-	}
-
-	c.Meta.Dir = filepath.Join(homeDir, ".influxdb/meta")
-	c.Data.Dir = filepath.Join(homeDir, ".influxdb/data")
-	c.Data.WALDir = filepath.Join(homeDir, ".influxdb/wal")
+	c.Meta.Dir = "./meta"
+	c.Data.Dir = "./data"
+	c.Data.WALDir = "./wal"
 
 	return c, nil
 }
